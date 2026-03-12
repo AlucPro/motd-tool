308eeeb5a3335420001924b9089e465fec3408c3
Xuming <jinxm.alucard@gmail.com>
2026-03-12 22:51:14 +0800

    chore: release v1.0.2

--- Formula/motd-tool.rb
+++ Formula/motd-tool.rb
@@ -1,8 +1,8 @@
 class MotdTool < Formula
   desc "Tiny CLI that prints a random message of the day in your terminal"
   homepage "https://github.com/AlucPro/motd-tool"
-  url "https://registry.npmjs.org/motd-tool/-/motd-tool-1.0.1.tgz"
-  sha256 "3657c4e0c647991a71437a1dd9ad5bec3e13d3becfa0a3fce2705c089b59460b"
+  url "https://registry.npmjs.org/motd-tool/-/motd-tool-1.0.2.tgz"
+  sha256 "fbb902fb18383c793db72870b16a7e7c0b84ca1977bf5b9155ae7308c334c7d3"
   license "MIT"
 
   depends_on "node"
